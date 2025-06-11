32c203129848e7179fdfa9fac0319408f37203d7
limpido <43034953+limpido@users.noreply.github.com>
2025-06-11 17:12:27 +0800

    [go] push with --no-thin

--- go/cmd/pick.go
+++ go/cmd/pick.go
@@ -12,9 +12,9 @@ func init() {
 }
 
 var pickCmd = &cobra.Command{
-	Use:   "pick <branch> [<branch>...]",
-	Short: "Cherrypick HEAD commit to the specified branch(es) on remote server",
-	Args:  cobra.MinimumNArgs(1),
+	Use:                   "pick <branch> [<branch>...]",
+	Short:                 "Cherrypick HEAD commit to the specified branch(es) on remote server",
+	Args:                  cobra.MinimumNArgs(1),
 	DisableFlagsInUseLine: true,
 	Run: func(cmd *cobra.Command, args []string) {
 		head := util.GitHead()
@@ -25,7 +25,7 @@ var pickCmd = &cobra.Command{
 			util.Execute(fmt.Sprintf("git checkout %s", branch))
 			util.Execute(fmt.Sprintf("git checkout -b %s", tmpBranch))
 			util.Execute(fmt.Sprintf("git cherry-pick %s", head))
-			util.Execute(fmt.Sprintf("git push %s HEAD:refs/for/%s", origin, branch))
+			util.Execute(fmt.Sprintf("git push %s HEAD:refs/for/%s --no-thin", origin, branch))
 			util.Execute(fmt.Sprintf("git checkout %s", curBranch))
 			util.Execute(fmt.Sprintf("git branch -D %s", tmpBranch))
 		}
